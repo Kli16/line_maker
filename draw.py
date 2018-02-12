35469e4c52b67addc85d64b23aa32eb158c54aa2
Kevin Li <kevinli@dwightshrute.home>
2018-02-12 00:18:07 -0500

    at least octant 1 and 2 are working

--- draw.py
+++ draw.py
@@ -5,11 +5,43 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     y = y0
     A = y1 - y
     B = x - x1
-    d = 2 * A + B
-    while (x < x1):
-        plot(screen, color, x, y)
-        if (d > 0):
+    #OcTanT I:
+    if (A > 0 and abs(B) > A):
+        d = 2 * A + B
+        while (x <= x1):
+            plot(screen, color, x, y)
+            if (d > 0):
+                y += 1
+                d += 2 * B
+            x += 1
+            d += 2 * A
+    #OcTanT II:
+    if (A > 0 and abs(B) < A):
+        d = A + 2 * B
+        while (y <= y1):
+            plot(screen, color, x, y)
+            if (d < 0):
+                x += 1
+                d += 2 * A
             y += 1
             d += 2 * B
-        x += 1
-        d += 2 * A
+    #OcTanT VIII:
+    if (A < 0 and abs(B) < abs(A)):
+        d = 2 * A - B
+        while (x <= x1):
+            plot(screen, color, x, y)
+            if (d < 0):
+                y += 1
+                d -= 2 * B
+            x += 1
+            d += 2 * A
+    #OcTanT VII:
+    if (A < 0 and abs(A) > abs(B)):
+        d = 2 * B - A
+        while (y <= y1):
+            plot(screen, color, x, y)
+            if (d > 0):
+                x += 1
+                d -= 2 * A
+            y += 1
+            d += 2 * B
--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-
+draw_line (0,0,500, 200, screen, color)
+draw_line (0,0,500, 100, screen, color)
+draw_line (0,0,200, 500, screen, color)
+draw_line (0,0,100, 500, screen, color)
+draw_line (0,500,500, 100, screen, color)
+#draw_line (0,500, 500, 400, screen, color)
 display(screen)
 save_extension(screen, 'img.png')
-
-draw_line (0,0,400, 200, screen, color)
