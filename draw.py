e669509fbd91acf1f2dde35c01fe06df0545ee77
Kevin Li <kevinli@dwightshrute.home>
2018-02-11 23:14:47 -0500

    added stuff

---
+++ draw.py
@@ -0,0 +1,15 @@
+from display import *
+
+def draw_line( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y
+    B = x - x1
+    d = 2 * A + B
+    while (x < x1):
+        plot(screen, color, x, y)
+        if (d > 0):
+            y += 1
+            d += 2 * B
+        x += 1
+        d += 2 * A
