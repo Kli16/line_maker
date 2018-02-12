fc258290f5966181566530a61922230d8806da8b
Kevin Li <kevinli@dwightshrute.home>
2018-02-12 00:53:48 -0500

    yay i finished

--- draw.py
+++ draw.py
@@ -5,43 +5,58 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     y = y0
     A = y1 - y
     B = x - x1
-    #OcTanT I:
-    if (A > 0 and abs(B) > A):
-        d = 2 * A + B
+    if (B == 0): #vertical line
+        if (y < y1):
+            while (y <= y1):
+                plot(screen, color, x, y)
+                y += 1
+        elif (y > y1):
+            while (y1 <= y):
+                plot(screen, color, x, y)
+                y -= 1
+    elif (A == 0): #horizontal line
         while (x <= x1):
             plot(screen, color, x, y)
-            if (d > 0):
-                y += 1
-                d += 2 * B
             x += 1
-            d += 2 * A
-    #OcTanT II:
-    if (A > 0 and abs(B) < A):
-        d = A + 2 * B
-        while (y <= y1):
-            plot(screen, color, x, y)
-            if (d < 0):
+    else:
+        slope = A / (B * -1)
+    #OcTanT I:
+        if (slope >= 0 and slope <= 1):
+            d = 2 * A + B
+            while (x <= x1):
+                plot(screen, color, x, y)
+                if (d > 0):
+                    y += 1
+                    d += 2 * B
                 x += 1
                 d += 2 * A
-            y += 1
-            d += 2 * B
-    #OcTanT VIII:
-    if (A < 0 and abs(B) < abs(A)):
-        d = 2 * A - B
-        while (x <= x1):
-            plot(screen, color, x, y)
-            if (d < 0):
+    #OcTanT II:
+        elif (slope > 1):
+            d = A + 2 * B
+            while (y <= y1):
+                plot(screen, color, x, y)
+                if (d < 0):
+                    x += 1
+                    d += 2 * A
                 y += 1
-                d -= 2 * B
-            x += 1
-            d += 2 * A
-    #OcTanT VII:
-    if (A < 0 and abs(A) > abs(B)):
-        d = 2 * B - A
-        while (y <= y1):
-            plot(screen, color, x, y)
-            if (d > 0):
+                d += 2 * B
+    #OcTanT VIII:
+        elif (slope < 0 and slope >= -1):
+            d = 2 * A - B
+            while (x <= x1):
+                plot(screen, color, x, y)
+                if (d < 0):
+                    y -= 1
+                    d -= 2 * B
                 x += 1
-                d -= 2 * A
-            y += 1
-            d += 2 * B
+                d += 2 * A
+    #OcTanT VII:
+        elif (slope < -1):
+            d = A - 2 * B
+            while (y >= y1):
+                plot(screen, color, x, y)
+                if (d > 0):
+                    x += 1
+                    d += 2 * A
+                y -= 1
+                d -= 2 * B
--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
-draw_line (0,0,500, 200, screen, color)
-draw_line (0,0,500, 100, screen, color)
-draw_line (0,0,200, 500, screen, color)
-draw_line (0,0,100, 500, screen, color)
-draw_line (0,500,500, 100, screen, color)
-#draw_line (0,500, 500, 400, screen, color)
+
+#angery eyebrows
+color = [ 205, 86, 57 ]
+draw_line (100,400,230, 300, screen, color)
+draw_line (270,300,400, 400, screen, color)
+#angery eyes
+color = [ 15, 86, 209 ]
+draw_line (150,270,200, 270, screen, color) #SLOPE 0
+draw_line (320,270,370, 270, screen, color) #SLOPE 0
+#angery nose
+color = [ 0, 153, 0]
+draw_line (250,200,250, 250, screen, color) #SLOPE UNDEFINED
+#angery mouth
+color = [ 205, 0, 0 ]
+draw_line (150,120, 380, 120, screen, color)
+draw_line (120,90, 150, 120, screen, color) #SLOPE 1
+draw_line (380,120, 410, 90, screen, color) #SLOPE -1
 display(screen)
 save_extension(screen, 'img.png')
